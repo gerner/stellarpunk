93f59c1e86e089f0f3778753c371ff1959c6291c
Nick Gerner <nick.gerner@gmail.com>
2024-12-30 16:33:54 -0600

    setting version

--- src/stellarpunk/_version.py
+++ src/stellarpunk/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.3.dev68+g9ad115e.d20241230'
-__version_tuple__ = version_tuple = (0, 0, 3, 'dev68', 'g9ad115e.d20241230')
+__version__ = version = '0.0.3.dev69+g66d1ee5'
+__version_tuple__ = version_tuple = (0, 0, 3, 'dev69', 'g66d1ee5')
